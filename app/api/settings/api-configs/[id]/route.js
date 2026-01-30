f402033cb48408eaa37c0871ace6eb7f32cec289
Tazminur Rahman <m2air@m2s-MacBook-Air.local>
2026-01-30 22:01:31 +0600

    Update Npm run build error

--- app/api/settings/api-configs/[id]/route.js
+++ app/api/settings/api-configs/[id]/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { getDb } from '../../../../lib/mongodb';
+import { getDb } from '../../../../../lib/mongodb';
 import { ObjectId } from 'mongodb';
 
 // PUT update API config
